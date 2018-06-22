0b854651bb7aee995faa22168cf35efc00bb7c61
Edward Nunez <edwardnnz@gmail.com>
2018-06-22 11:07:04 -0400

    added github authentication

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -32,24 +32,48 @@ firebase.initializeApp(config);
 //Reference to the database
 var database = firebase.database();
 
-//Reference to data storage location for player objects
-var playersRef = database.ref("/players");
+// //Reference to data storage location for player objects
+// var playersRef = database.ref("/players");
 
-//Reference special location of client connection state
-var connectedRef = database.ref(".info/connected");
+// //Reference special location of client connection state
+// var connectedRef = database.ref(".info/connected");
 
-//When the client's connection state changes
-connectedRef.on("value", function(snapshot) {
-    //If they are connected
-    if (snapshot.val()) {
-        //Add user to the connections list
-        var con = playersRef.push(true);
-        //Remove user from the connection list when they disconnect
-        con.onDisconnect().remove()
-    }
-});
+// //When the client's connection state changes
+// connectedRef.on("value", function(snapshot) {
+//     //If they are connected
+//     if (snapshot.val()) {
+//         //Add user to the connections list
+//         var con = playersRef.push(true);
+//         //Remove user from the connection list when they disconnect
+//         con.onDisconnect().remove()
+//     }
+// });
 
-//Function for when user connects to app, displays total users in html
-playersRef.on("value", function(snapshot) {
-    $("body").append("<strong>" + snapshot.numChildren() + "</strong>");
-})
+// //Function for when user connects to app, displays total users in html
+// playersRef.on("value", function(snapshot) {
+//     $("body").append("<strong>" + snapshot.numChildren() + "</strong>");
+// })
+
+
+function githubLogin() {
+    const provider = new firebase.auth.GithubAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const token = result.credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+        $(".navbar").write(`Hello ${user.displayName}`);
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(errorCode);
+      });
+}
