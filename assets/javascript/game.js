e8d734bddcf7f4c32304cf15ce54c3f0fea27796
dotRollen <edwardnnz@gmail.com>
2018-08-04 14:09:48 -0400

    finished game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,289 @@
-// Game
 
-var STATE = {OPEN: 1, JOINED: 2, };
+"use strict";
 
-// {
-//     "creator": {"displayName": "", "uid": ""},
-//     "joiner": {"displayName": "", "uid": ""},
-//     "state": 2,
-// }
+var Game = (function() {
 
-ref = firebase.database().ref("/games");
+    var ref;
+    var create;
+    var creator = {};
+    var joiner = {};
+    var gameList;
+    var arena;
+    var refereeTimer;
 
-function createGame() {
-    var user = firebase.auth().currentUser;
-    var currentGame = {
-        creator: {uid: user.uid, displayName: user.displayName},
-        state: STATE.OPEN
+    var STATE = {OPEN: 1, JOINED: 2, CHOICE: 3, READY: 4, BATTLE: 5, COMPLETE: 6};
+    var CHOICES = {
+        ROCK: {label: "Rock"},
+        PAPER: {label: "Paper"},
+        SCISSORS: {label: "Scissors"}
     };
 
-    ref.push().set(currentGame);
-}
-
-function joinGame(key) {
-    var user = firebase.auth().currentUser;
-    var gameRef = ref.child(key);
-    gameRef.transaction(function(game) {
-        if (!game.joiner) {
-            game.state = STATE.JOINED;
-            game.joiner = {uid: user.uid, displayName: user.displayName}
-        }
-        return game;
-    })
-}
-
-ref = firebase.database().ref("/games");
-var openGames = ref.orderByChild("state").equalTo(STATE.OPEN);
-openGames.on("child_added", function(snapshot) {
-    var data = snapshot.val();
-
-    if (data.creator.uid != firebase.auth().currentUser.uid) {
-        addJoinGameButton(snapshot.key, data);
+    function enableCreateGame(enabled) {
+        create.disabled = !enabled;
     }
-});
 
-openGames.on("child_removed", function(snapshot) {
-    var item = $("#" + snapshot.key);
-    if (item) {
-        item.remove();
+    function addJoinGameButton(key, game) {
+        var item = document.createElement("li");
+        item.id = key;
+        item.innerHTML = '<button ' +
+            'class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button-accent">' +
+            'Join ' + game.creator.displayName + '</button>';
+        item.addEventListener("click", function() {
+            joinGame(key);
+        });
+
+        gameList.appendChild(item);
     }
-});
 
-function watchGame(key) {
-    var gameRef = ref.child(key);
-    gameRef.on("value", function(snapshot) {
-        var game = snapshot.val();
-        switch (game.state) {
-            case STATE.JOINED: joinedGame(gameRef, game); break;
-            case STATE.TAKE_PICTURE: countDowntoTakePicture(gameRef, game); break;
-            case STATE.UPLOADED_PICTURE: displayUploadedPicture(game);
-                                         detectMyFace(gameRef, game); break;
-            case STATE.FACE_DETECTED: displayDetectedEmotion(game);
-                                      determineWinner(gameRef, game); break;
-            case STATE.COMPLETE: showWinner(game); break;
+    function createGame() {
+        enableCreateGame(false);
+
+        var user = firebase.auth().currentUser;
+        creator = {
+            uid: user.uid,
+            displayName: user.displayName,
+            choice: null,
+        };
+        var currentGame = {
+            creator: creator,
+            state: STATE.OPEN
+        };
+
+        var key = ref.push();
+        key.set(currentGame, function(error) {
+            if(error) {
+                UI.snackbar({message: "Error creating game"});
+            } else {
+                key.onDisconnect().remove();
+                gameList.style.display = "none";
+                watchGame(key.key);
+            }
+        })
+    };
+
+    function joinGame(key) {
+        console.log("Attempting to join game: ", key);
+        var user = firebase.auth().currentUser;
+        joiner = {
+            uid: user.uid,
+            displayName: user.displayName,
+            choice: null,
+        };
+
+        ref.child(key).transaction(function(game) {
+            if (!game.joiner) {
+                game.state = STATE.JOINED;
+                game.joiner = joiner
+            }
+            return game;
+        }, function(error, committed, snapshot) {
+            if (committed) {
+                if (snapshot.val().joiner.uid == user.uid) {
+                    enableCreateGame(false);
+                    watchGame(key);
+                } else {
+                    UI.snackbar({message: "Game already joined. Please choose another."});
+                }
+            } else {
+                console.log("Could not commit when trying to join game", error);
+                UI.snackbar({message: "Error joining game"});
+            }
+        })
+    };
+
+    function joinedGame(gameRef, game) {
+        if (game.creator.uid == firebase.auth().currentUser.uid) {
+            UI.snackbar({message: game.joiner.displayName + " has joined your game."});
+            //wait a little bit
+            window.setTimeout(function() {
+                gameRef.update({state: STATE.CHOICE});
+            }, 1000);
         }
-    })
-}
+    }
 
-function takePicture(gameRef, game) {
-    var canvas = $("<canvas>");
-    canvas.attr("width", "640");
-    canvas.attr("height", "480");
-    var context = canvas.getContext("2d");
-    var cam = $("#cam");
-    context.drawImage(cam, 0, 0, canvas.attr("width"), canvas.attr("height"));
+    function countDowntoChoice(gameRef, game) {
+        var data = {};
+        if (game.creator.uid == firebase.auth().currentUser.uid) {
+            data = {
+                state: STATE.READY,
+                creator: creator
+            };
+        } else {
+            data = {
+                state: STATE.READY,
+                joiner: joiner
+            };
+        };
+        var count = 10;
+        refereeTimer.innerHTML = count;
+        results.style.display = "none";
+        arena.style.display = "block";
 
-    canvas.toBlob(function(blob) {
+        resetArena(game);
+        
+        var countDown = function() {
+            if (count >= 1 ) {
+                count--;
+                refereeTimer.innerHTML = count;
+                setTimeout(countDown, 1000)
+            } else {
+                window.setTimeout(function() {
+                    gameRef.update(data);
+                }, 1000);
+            }
+        }
+        setTimeout(countDown, 1000);
+    };
 
-    });
-}
+    function resetArena(game) {
+        var player1 = document.querySelector("#player-1");
+        var player2 = document.querySelector("#player-2");
+        var choices = document.getElementsByClassName("choice");
+        disableChoices(choices, false);
+ 
+        Array.from(choices).forEach(function(element) {
+            element.addEventListener('click', function(event) {
+                var choice = event.target.getAttribute("data-choice");
+                disableChoices(choices, true);
+                if(game.creator.uid == firebase.auth().currentUser.uid){
+                    creator.choice = choice;
+                } else {
+                    joiner.choice = choice;
+                }
+            });
+        });
+
+        if (game.creator.uid == firebase.auth().currentUser.uid) {
+            player2.innerHTML = "You are Player 1";
+        } else {
+            player1.innerHTML = "You are Player 2";
+        }
+    }
+
+    function disableChoices(choices, enabled){
+        if(enabled) {
+            Array.from(choices).forEach(function(element) {
+                element.disabled = enabled;
+            })
+        } else {
+            Array.from(choices).forEach(function(element) {
+                element.disabled = enabled;
+            })
+        }
+    }
+
+    function displayChoice(game){
+        arena.style.display = "none";
+        results.style.display = "block";
+        var playerOne = game.creator;
+        var playerTwo = game.joiner;
+
+        if (playerOne.choice == playerTwo.choice ) {
+            results.innerHTML = `
+            <h4>Results</h4>
+            <p>${ playerOne.displayName }: ${ playerOne.choice }</p>
+            <p>${ playerTwo.displayName }: ${ playerTwo.choice }</p>
+            <h1>TIE!</h1>
+            `;
+        }
+
+        else if (playerOne.choice === "rock" && playerTwo.choice === "paper" ){ 
+            results.innerHTML = `
+            <h4>Results</h4>
+            <p>${ playerOne.displayName }: ${ playerOne.choice }</p>
+            <p>${ playerTwo.displayName }: ${ playerTwo.choice }</p>
+            <h1>${ playerTwo.displayName } Wins!</h1>
+            `;
+        }
+
+        else if (playerOne.choice === "paper" && playerTwo.choice === "scissor"){
+            results.innerHTML = `
+            <h4>Results</h4>
+            <p>${ playerOne.displayName }: ${ playerOne.choice }</p>
+            <p>${ playerTwo.displayName }: ${ playerTwo.choice }</p>
+            <h1>${ playerTwo.displayName } Wins!</h1>
+            `;
+        }
+
+        else if (playerOne.choice === "scissor" && playerTwo.choice === "rock"){
+            results.innerHTML = `
+            <h4>Results</h4>
+            <p>${ playerOne.displayName }: ${ playerOne.choice }</p>
+            <p>${ playerTwo.displayName }: ${ playerTwo.choice }</p>
+            <h1>${ playerTwo.displayName } Wins!</h1>
+            `;
+        }
+        
+        else {
+            results.innerHTML = `
+            <h4>Results</h4>
+            <p>${ playerOne.displayName }: ${ playerOne.choice }</p>
+            <p>${ playerTwo.displayName }: ${ playerTwo.choice }</p>
+            <h1>${ playerOne.displayName } Wins!</h1>
+            `;
+        }
+    }
+
+    function watchGame(key) {
+        var gameRef = ref.child(key);
+        gameRef.on("value", function(snapshot) {
+            var game = snapshot.val();
+            console.log("Game update:", game);
+
+            //if we get a null value, because remove - ignore it.
+            if (!game) {
+                UI.snackbar({message: "Game has finished. Please play again."});
+                enableCreateGame(true);
+                return
+            }
+
+            switch (game.state) {
+                case STATE.JOINED:
+                    console.log("state: joined");
+                    joinedGame(gameRef, game);
+                    break;
+                case STATE.CHOICE:
+                    console.log("state: choice");
+                    countDowntoChoice(gameRef, game);
+                    break;
+                case STATE.READY:
+                    console.log("state: ready");
+                    displayChoice(game);
+                    break;
+            }
+        })
+    }
+
+    return {
+        init: function() {
+            create = document.querySelector("#create-game");
+            create.addEventListener("click", createGame);
+            arena = document.querySelector("#arena");
+            gameList = document.querySelector("#games ul");
+            refereeTimer = document.querySelector("#referee #timer");
+            
+            ref = firebase.database().ref("/games");
+
+            var openGames = ref.orderByChild("state").equalTo(STATE.OPEN);
+            openGames.on("child_added", function(snapshot) {
+                var data = snapshot.val();
+                console.log("Game Added:", data);
+
+                if (data.creator.uid != firebase.auth().currentUser.uid) {
+                    addJoinGameButton(snapshot.key, data);
+                }
+            });
+
+            openGames.on("child_removed", function(snapshot) {
+                var item = document.querySelector("#" + snapshot.key);
+                if (item) {
+                    item.remove();
+                }
+            });
+        },
+
+        onlogin: function() {
+            enableCreateGame(true);
+        }
+    }
+})();
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,194 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCBUHs2G8hK3_U-GqYbk5LSTG_p33BmWfc",
-    authDomain: "rock-paper-scissors-5cb1c.firebaseapp.com",
-    databaseURL: "https://rock-paper-scissors-5cb1c.firebaseio.com",
-    projectId: "rock-paper-scissors-5cb1c",
-    storageBucket: "",
-    messagingSenderId: "640511078897"
+    apiKey: "AIzaSyAAP4q4Ot6jtVGW8obXh64XHO4AAsdsZFQ",
+    authDomain: "janken-joust.firebaseapp.com",
+    databaseURL: "https://janken-joust.firebaseio.com",
+    projectId: "janken-joust",
+    storageBucket: "janken-joust.appspot.com",
+    messagingSenderId: "170983198405"
 };
 
 firebase.initializeApp(config);
+
+"use strict";
+
+var UI = (function() {
+
+    return {
+        snackbar: function(data) {
+            document.querySelector("#snackbar").MaterialSnackbar.showSnackbar(data);
+        },
+    }
+}());
+
+var Session = (function() {
+    var loggedIn = false;
+
+    function authStateChangeListener(user) {
+        console.log("Auth state change: ", user);
+        if (user) {
+            loggedIn = true;
+            closeLoginDialog();
+            document.querySelector("#login-btn").style.display = "none";
+            document.querySelector("#logout-btn").style.display = "block";
+            document.querySelector("#login-message").innerHTML = "Hello, " + user.displayName + "!";
+
+            Chat.onlogin();
+            Game.onlogin();
+
+        } else {
+            if (loggedIn) {
+                loggedIn = false;
+                window.location.reload();
+                [send, messageField].forEach(function(item) {
+                    item.disabled = !enable;
+                })
+            }
+        }
+    }
+
+    function signInWithEmailandPassword() {
+        var email = document.querySelector("#email");
+        var password = document.querySelector("#password");
+
+        firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(function(user) {
+            console.log("Signed in with user: ", user);
+        }, function(error) {
+            console.log("Sign in error: ", error);
+        })
+    }
+
+    function closeLoginDialog() {
+        var dialog = document.querySelector("#login-dialog");
+        if (dialog.open) {
+            dialog.close();
+        }
+    }
+    
+    // function userAvatar(email) {
+
+    // }
+
+    function submitCreateAccount() {
+        //fields
+        var displayName = document.querySelector("#entry-displayname");
+        var email = document.querySelector("#entry-email");
+        var password = document.querySelector("#entry-password");
+
+        firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+        .then(function() {
+            user = firebase.auth().currentUser;
+            console.log('Create user and sign in Success', user);
+            //add the displayName
+            user.updateProfile({
+                displayName: displayName.value,
+                // photoURL: userAvatar(email);
+            });
+            createDialog.close();
+            closeLoginDialog();
+        }, function(error) {
+            console.error('Create user and sign in Error', error);
+            createDialog.close();
+            closeLoginDialog();
+        });
+    }
+
+    return {
+        init: function() {
+            firebase.auth().onAuthStateChanged(authStateChangeListener);
+
+            //login
+            var loginDialog = document.querySelector("#login-dialog");
+            dialogPolyfill.registerDialog(loginDialog);
+            document.querySelector("#login-btn").addEventListener("click", function() {
+                loginDialog.showModal();
+            });
+            document.querySelector("#sign-in").addEventListener("click", signInWithEmailandPassword);
+            
+            //logout
+            document.querySelector("#logout-btn").addEventListener("click", function() {
+                firebase.auth().signOut().then(function() {
+                    console.log('Signed Out');
+                }, function(error) {
+                    console.error('Sign Out Error', error);
+                });
+            });
+            
+            //create accounts
+            createDialog = document.querySelector("#create-account-dialog");
+            dialogPolyfill.registerDialog(createDialog);
+            document.querySelector("#create-account").addEventListener("click", function() {
+                createDialog.showModal();
+            });
+            document.querySelector("#entry-submit").addEventListener("click", submitCreateAccount);
+
+        },
+    }
+}());
+
+var Chat = (function() {
+    var send;
+    var messageField;
+    var messages;
+
+    function enableChat(enable) {
+        console.log("enabling chat: ", enable);
+        [send, messageField].forEach(function(item) {
+            item.disabled = !enable;
+        })
+    }
+    function sendChatMessage() {
+        enableChat(false);
+        chatRef.push().set({
+            name: firebase.auth().currentUser.displayName,
+            message: messageField.value
+        }, function(error) {
+            if(error) {
+                console.log("Error saving data", error);
+                UI.snackbar({message: "Error sending message"});
+            } else {
+                messageField.value = "";
+                messageField.parentElement.classList.remove("is-dirty");
+            }
+
+            enableChat(true);
+        })
+    }
+
+    function addChatMessage(name, message) {
+        var item = document.createElement("li");
+        item.innerHTML = ("<strong>" + name + ":</strong> " + message );
+
+        var messageList = messages.querySelector("ul");
+        messageList.appendChild(item);
+        messages.scrollTop = messageList.scrollHeight;
+
+    };
+
+    return {
+        init: function() {
+            send = document.querySelector("#send-chat");
+            messageField = document.querySelector("#chat-input");
+            messages = document.querySelector("#chat-messages");
+            
+            chatRef = firebase.database().ref("/chat");
+
+            send.addEventListener("click", sendChatMessage);
+            
+            chatRef.on("child_added", function(snapshot) {
+                var message = snapshot.val();
+                addChatMessage(message.name, message.message);
+            });
+        },
+
+        onlogin: function() {
+            enableChat(true);
+        }
+    }
+})();
+
+window.onload = function() {
+    Session.init();
+    Chat.init();
+    Game.init();
+};
